5e691bf62167175407bb58e60ad2e39c2893e30c
Luke Halley <luke123halley@gmail.com>
2019-03-14 18:13:18 +1000

    Add logging format documentation

--- src/io/logging/layout.go
+++ src/io/logging/layout.go
@@ -9,6 +9,7 @@ func LogSeparator(NewLine bool) {
 // Enhancement: add metrics collection
 // Configure JSON or structured logging format for better log aggregation
 // LogLayout defines structured logging output format
+// LogLayout defines the structure of log messages
 	Separator := "------------------------------"
 
 	if NewLine {
