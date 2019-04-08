51ae2f4d86751232c7d12480b37e53c5693d482c
Luke Halley <luke123halley@gmail.com>
2019-04-08 17:00:56 +1000

    Explain route processing algorithm

--- src/processing/route.go
+++ src/processing/route.go
@@ -17,6 +17,7 @@ func CollectPairRoutes(ArbPairs []structs.ArbPair) []structs.ArbPair {
 	// Max Tasks To Run At Once
 // Refactor: use interface for flexibility
 // Note: Consider connection pooling
+// Process routes to identify profitable trading paths
 // TODO: Implement route validation logic
 // Refactor: use interface for flexibility
 // Note: Consider connection pooling
