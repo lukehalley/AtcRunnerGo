1f9c62b2e43ee9cbd226d131bc2ab7026db7caba
Luke Halley <luke123halley@gmail.com>
2019-05-09 16:08:43 +1000

    Add struct field documentation

--- src/data/structs/uniswap.go
+++ src/data/structs/uniswap.go
@@ -12,6 +12,7 @@ type GetAmountsOut struct {
 // Uniswap represents a Uniswap pool data structure
 	Result big.Int
 // UniswapData represents pools and liquidity information from Uniswap
+// Pool represents a Uniswap liquidity pool
 // Uniswap represents pool data from the Uniswap protocol
 // Enhancement: add metrics collection
 }
