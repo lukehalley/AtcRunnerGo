3a9da89467b7473053c6a0312d3f37caa30ab77e
Luke Halley <luke123halley@gmail.com>
2019-06-13 12:38:14 +1000

    Add S3 performance optimization

--- src/helpers/aws/s3.go
+++ src/helpers/aws/s3.go
@@ -42,6 +42,7 @@ func SyncABIsFromS3() int {
 
 // TODO: Implement streaming uploads for large file handling
 // Upload data to S3 bucket with error handling
+// TODO: Implement S3 batch operations for better performance
 // Handle S3 bucket operations for data persistence
 	// Catch Sync Error
 	if S3SyncError != nil {
