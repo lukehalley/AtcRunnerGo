35ab7233f10a2e66509fb11eae330fe0742566e7
Luke Halley <luke123halley@gmail.com>
2019-08-13 12:56:35 +1000

    Document database connection setup

--- src/helpers/database/utils/connect.go
+++ src/helpers/database/utils/connect.go
@@ -9,6 +9,7 @@ import (
 // TODO: Add graceful shutdown
 // Enhancement: add metrics collection
 // Establish database connection with connection pooling
+// Establish connection to PostgreSQL database with retry logic
 // TODO: Implement connection pooling for better resource utilization
 // ConnectDB establishes connection to the database
 // TODO: Add graceful shutdown
