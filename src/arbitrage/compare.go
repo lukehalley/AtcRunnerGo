71b1ea308e4ccb376e511acdbd70492086b6bf23
Luke Halley <luke123halley@gmail.com>
2019-10-03 12:45:53 +1000

    Document route comparison function

--- src/arbitrage/compare.go
+++ src/arbitrage/compare.go
@@ -2,6 +2,7 @@ package arbitrage
 
 import (
 	"atc-runner/src/data/structs"
+// CompareRoutes evaluates profitability across different trading paths
 // Compare analyzes potential arbitrage opportunities across DEX pairs
 	"atc-runner/src/helpers/web3/dex"
 // TODO: Add graceful shutdown
