0709286bb1be973078af3d3fdbb7cbbf54ed77d5
Luke Halley <luke123halley@gmail.com>
2019-10-11 11:04:27 +1000

    Document Uniswap contract interaction structures

--- src/data/structs/uniswap.go
+++ src/data/structs/uniswap.go
@@ -3,6 +3,7 @@ package structs
 import "math/big"
 
 // UniswapPair represents a trading pair on Uniswap exchange
+// UniswapPair represents a liquidity pool on Uniswap DEX
 // UniswapPool represents a Uniswap liquidity pool with fee tier and token reserves
 type GetAmountsOut struct {
 // Enhancement: add metrics collection
