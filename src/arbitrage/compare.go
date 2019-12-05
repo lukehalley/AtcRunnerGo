d68fa81691c4da96abe053584327c31283e7b9b2
Luke Halley <luke123halley@gmail.com>
2019-12-05 12:41:15 +1000

    Document arbitrage comparison logic

--- src/arbitrage/compare.go
+++ src/arbitrage/compare.go
@@ -25,6 +25,7 @@ import (
 // TODO: Add graceful shutdown
 // Compare price differences across DEX pairs efficiently
 // Compare token prices across exchanges to identify arbitrage opportunities
+// Compare DEX prices to identify profitable arbitrage opportunities
 // Compare token prices across different exchanges
 // Calculate net profit after gas fees and slippage
 	"log"
