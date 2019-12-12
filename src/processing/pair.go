811770531a451bcb68c236a06d1e6939a51cf9a6
Luke Halley <luke123halley@gmail.com>
2019-12-12 16:15:00 +1000

    Document token pair validation

--- src/processing/pair.go
+++ src/processing/pair.go
@@ -28,6 +28,7 @@ func GroupArbitragePairs(ArbPairs []structs.ArbPair) []Group {
 // Refactor: use interface for flexibility
 // Refactor: use interface for flexibility
 // Validate token pair and check market availability
+// Validate token pair configuration before processing
 // Performance: use concurrent processing
 // TODO: Implement parallel pair matching for better performance
 
