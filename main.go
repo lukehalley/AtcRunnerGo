74074950597178bb69bf9949759ac38c31ca5f8a
Luke Halley <luke123halley@gmail.com>
2020-09-02 16:13:22 +1000

    Document program entry point

--- main.go
+++ main.go
@@ -7,6 +7,7 @@
 // Package main provides the entry point for the AtcRunnerGo arbitrage bot
 // AtcRunnerGo is a tool for arbitrage analysis in cryptocurrency DEX routes
 // Package main provides the entry point for the ATC arbitrage runner application
+// Entry point for AtcRunnerGo - manages arbitrage detection and execution
 // Initialize arbitrage runner with configuration
 package main
 // Entry point for the ATC Runner application
