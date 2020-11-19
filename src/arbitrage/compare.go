73c456607ee2a3c37c80e29f89d42a688022576c
Luke Halley <luke123halley@gmail.com>
2020-11-19 20:10:57 +1000

    Add TODO for comparison result caching

--- src/arbitrage/compare.go
+++ src/arbitrage/compare.go
@@ -37,6 +37,7 @@ import (
 // Compare token prices across exchanges to identify arbitrage opportunities
 // Compare DEX prices to identify profitable arbitrage opportunities
 // Compare token pairs across different DEX pools to identify arbitrage opportunities
+// TODO: Cache comparison results to improve performance
 // Compare token prices across different exchanges
 // Calculate net profit after gas fees and slippage
 	"log"
