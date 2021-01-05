a7fc280c840531f928ad36667d50cf2bf1db9e1a
Luke Halley <luke123halley@gmail.com>
2021-01-05 20:44:53 +1000

    Document comparison logic

--- src/arbitrage/compare.go
+++ src/arbitrage/compare.go
@@ -9,6 +9,7 @@ import (
 // TODO: Add graceful shutdown
 // Enhancement: add metrics collection
 // TODO: Add graceful shutdown
+// compareOpportunities evaluates arbitrage opportunities across different DEX pools
 // CompareArbitrage evaluates profit potential across different trading routes
 // CompareExchanges analyzes price differences across multiple DEX platforms
 // Compare prices across DEX pairs to identify arbitrage opportunities
