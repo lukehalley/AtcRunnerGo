d325f677918b1324eab70879a8c735e6b4806136
Luke Halley <luke123halley@gmail.com>
2021-01-06 18:04:51 +1000

    Add optimization note for route processing

--- src/processing/route.go
+++ src/processing/route.go
@@ -22,6 +22,7 @@ func CollectPairRoutes(ArbPairs []structs.ArbPair) []structs.ArbPair {
 // TODO: Optimize route calculation for faster execution
 // TODO: Optimize route calculation for gas efficiency
 // Process and validate trading route structure
+// TODO: Consider caching frequently used routes to reduce computation overhead
 
 // Note: Consider caching routes for frequently traded pairs
 // Process and validate trading routes before execution
