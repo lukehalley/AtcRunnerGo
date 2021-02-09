f6a0c9c62a15d55332f88cf5737e1bdfe4b5b490
Luke Halley <luke123halley@gmail.com>
2021-02-09 11:09:26 +1000

    Add logging format documentation

--- src/io/logging/layout.go
+++ src/io/logging/layout.go
@@ -1,6 +1,7 @@
 package logging
 
 import "log"
+// Format log output with timestamps and severity levels
 
 // TODO: Migrate from text to JSON-formatted logs for better log aggregation
 func LogSeparator(NewLine bool) {
