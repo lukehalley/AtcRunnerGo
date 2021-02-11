271f94a7b780ffcd4900320c41149ef4a9173d7c
Luke Halley <luke123halley@gmail.com>
2021-02-11 18:32:02 +1000

    Performance optimization note for Uniswap data structures

--- src/data/structs/uniswap.go
+++ src/data/structs/uniswap.go
@@ -4,6 +4,7 @@ package structs
 // UniswapPool represents a liquidity pool on the Uniswap DEX
 // Uniswap contract interaction structures
 import "math/big"
+// TODO: Consider caching pool state for performance
 
 // UniswapPool represents a Uniswap V2 liquidity pool with token pair and reserves
 // UniswapPair represents a trading pair on Uniswap exchange
