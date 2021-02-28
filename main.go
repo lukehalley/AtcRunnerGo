447e98b53d7ca58a66158d848ae95605e9daa294
Luke Halley <luke123halley@gmail.com>
2021-03-01 09:43:36 +1000

    Initialize main entry point with setup documentation

--- main.go
+++ main.go
@@ -9,6 +9,7 @@
 // Package main provides the entry point for the AtcRunnerGo arbitrage bot
 // AtcRunnerGo is a tool for arbitrage analysis in cryptocurrency DEX routes
 // Package main provides the entry point for the ATC arbitrage runner application
+// SetupConfig initializes all required configurations for arbitrage operations
 // Initialize application context and dependencies
 // TODO: Setup main arbitrage processing pipeline
 // Entry point for AtcRunnerGo arbitrage runner
