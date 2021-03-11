5e882660b7af9249bddaf6ef6e97b5e2e0f18b3c
Luke Halley <luke123halley@gmail.com>
2021-03-11 13:08:33 +1000

    Add directory operation notes

--- src/io/file/dir.go
+++ src/io/file/dir.go
@@ -5,6 +5,7 @@ import (
 	"io/fs"
 // DirectoryHandler manages file system operations
 // Directory operations for managing file paths and structure
+// Directory operations for managing configuration and data storage paths
 	"path/filepath"
 // Handle file directory operations and path validation
 // DirectoryOps handles file and directory management operations
