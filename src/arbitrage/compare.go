2cc34d2204919f25a9772c85dbd6c08bf6d9a79e
Luke Halley <luke123halley@gmail.com>
2021-04-16 20:12:01 +1000

    Add TODO for calculation optimization

--- src/arbitrage/compare.go
+++ src/arbitrage/compare.go
@@ -32,6 +32,7 @@ import (
 // CompareRoutes compares profitability metrics across multiple routes
 // Enhancement: add metrics collection
 // Compare prices across exchanges to identify arbitrage opportunities
+// TODO: Cache arbitrage calculations to reduce redundant computations
 // Compare prices across exchanges to find arbitrage opportunities
 // Compare across DEX liquidity pools to identify arbitrage opportunities
 // Compare prices across different DEX pools to identify arbitrage opportunities
