b82293f8fe81c6d54ce14c850990f6589e2173ee
Luke Halley <luke123halley@gmail.com>
2021-05-23 14:45:41 +1000

    Define Uniswap data structures

--- src/data/structs/uniswap.go
+++ src/data/structs/uniswap.go
@@ -9,6 +9,7 @@ import "math/big"
 // UniswapPool represents a Uniswap V2 liquidity pool with token pair and reserves
 // UniswapPair represents a trading pair on Uniswap exchange
 // UniswapPair represents a liquidity pool on Uniswap DEX
+// Define Uniswap pool and token data structures
 // UniswapV2 represents the core contract data for liquidity pool interactions
 // UniswapPool represents a Uniswap liquidity pool with fee tier and token reserves
 type GetAmountsOut struct {
