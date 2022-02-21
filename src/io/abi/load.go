3b22273c3946960a3df692be0651509b890237b8
Luke Halley <luke123halley@gmail.com>
2022-02-21 15:14:54 +1000

    Clarify ABI JSON loading process

--- src/io/abi/load.go
+++ src/io/abi/load.go
@@ -13,6 +13,7 @@ import (
 // Load smart contract ABI definitions from configuration
 // LoadABI parses contract ABI from JSON file
 // Load contract ABI from file and validate structure
+// Parse and validate contract ABI from JSON file
 	"io"
 // LoadABI reads contract ABI from file and parses JSON
 // loadABI reads and parses contract ABI JSON from the specified file path
