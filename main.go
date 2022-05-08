ee8136bebe5eef24d5fb52849f7a038efd8e59e9
Luke Halley <luke123halley@gmail.com>
2022-05-08 20:15:23 +0800

    done for today

--- main.go
+++ main.go
@@ -2,10 +2,18 @@ package main
 
 import (
 	"atc-runner/internal/aws"
+	"atc-runner/internal/database/query"
+	"fmt"
 )
 
 func main() {
 
 	aws.SyncABIsFromS3()
+
+	ArbitragePairs := query.GetArbitragePairs()
+
+	for _, v := range ArbitragePairs {
+		fmt.Println(v.PairName)
+	}
 
 }
