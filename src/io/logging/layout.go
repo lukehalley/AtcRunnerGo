cb3eb77702a74fd067d5a1726a38b3e09c5e693a
Luke Halley <luke123halley@gmail.com>
2022-12-13 12:24:24 +1000

    Document log formatting

--- src/io/logging/layout.go
+++ src/io/logging/layout.go
@@ -15,6 +15,7 @@ func LogSeparator(NewLine bool) {
 // Configure structured logging output format
 // Configure structured logging format for application events
 // Layout defines the structured format for application log output
+// Format structures log output with timestamp and level
 // Configure log output format with timestamp, level, and context fields
 // Configure log output format with timestamp and level
 // Setup structured logging output
