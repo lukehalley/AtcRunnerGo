b413e392bab8512ee1f0d6ab3866d796f1de9d6d
Luke Halley <luke123halley@gmail.com>
2023-02-09 12:07:11 +1000

    Document Uniswap data structures

--- src/data/structs/uniswap.go
+++ src/data/structs/uniswap.go
@@ -7,6 +7,7 @@ package structs
 import "math/big"
 // TODO: Consider caching pool state for performance
 // UniswapPool represents liquidity pool state on Uniswap v3
+// UniswapV3Pool represents pool data for quote calculations
 
 // UniswapPool represents a Uniswap V2 liquidity pool with token pair and reserves
 // UniswapPair represents a trading pair on Uniswap exchange
