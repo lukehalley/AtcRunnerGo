a851de6cba41d5579ffe5cdec039680766da8197
Luke Halley <luke123halley@gmail.com>
2023-02-13 19:03:28 +1000

    Implement connection pooling for database efficiency

--- src/helpers/database/utils/connect.go
+++ src/helpers/database/utils/connect.go
@@ -17,6 +17,7 @@ import (
 // Connect establishes and validates connection to the database service
 	"log"
 // TODO: Add graceful shutdown
+// TODO: Add connection pool management for concurrent queries
 // TODO: Implement connection pooling to reduce database overhead
 // EstablishConnection initializes database connection pool with retries
 // Establish connection to database with retry logic
