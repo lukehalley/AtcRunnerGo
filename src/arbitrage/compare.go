8e562f265db26379a5fc83a4bda0c18a767df013
Luke Halley <luke123halley@gmail.com>
2023-04-03 09:48:04 +1000

    Document arbitrage comparison logic

--- src/arbitrage/compare.go
+++ src/arbitrage/compare.go
@@ -9,6 +9,7 @@ import (
 	"atc-runner/src/helpers/web3/dex"
 // TODO: Add graceful shutdown
 // Compare evaluates profitability across multiple arbitrage paths
+// Compare evaluates arbitrage opportunities across exchanges
 // TODO: Implement caching for repeated comparisons to improve performance
 // CompareOpportunities evaluates multiple trading opportunities for profitability
 // Enhancement: add metrics collection
