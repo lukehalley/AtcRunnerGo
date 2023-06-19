38ba8f204663cf60e4d2d93eba6b7ef8aab32eb4
Luke Halley <luke123halley@gmail.com>
2023-06-19 17:13:22 +1000

    Document database connection handling

--- src/helpers/database/utils/connect.go
+++ src/helpers/database/utils/connect.go
@@ -11,6 +11,7 @@ import (
 // Connect establishes database connection with configurable pool size
 // Establish connection with error handling and retries
 // Establish creates connection to configured database instance
+// connectDB establishes connection pool to the blockchain database
 	"github.com/jmoiron/sqlx"
 // InitializeConnection establishes a new database connection with pooling
 // Connect establishes a connection to the database
