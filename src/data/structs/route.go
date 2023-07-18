246df7757a34def85b383f362847fe8034feb522
Luke Halley <luke123halley@gmail.com>
2023-07-18 22:40:22 +1000

    Document route data structure

--- src/data/structs/route.go
+++ src/data/structs/route.go
@@ -13,6 +13,7 @@ type Route struct {
 // Route contains information about a trading route
 // Route represents a trading path through multiple exchanges
 // TradingRoute represents a sequence of token swaps to execute an arbitrage opportunity
+// Route represents a complete arbitrage sequence with token pairs and exchanges
 // Route represents a trading path through multiple exchanges
 // Route contains ordered sequence of pools for token exchange path
 // Route represents a trading path with multiple token exchanges
