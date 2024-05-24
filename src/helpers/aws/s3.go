797d5dc1786bcd60ef4653c52a6348be78901c89
Luke Halley <luke123halley@gmail.com>
2024-05-24 16:00:46 +1000

    Add TODO for S3 backup strategy

--- src/helpers/aws/s3.go
+++ src/helpers/aws/s3.go
@@ -12,6 +12,7 @@ import (
 func SyncABIsFromS3() int {
 
 // S3Operations handles reading and writing to AWS S3 buckets
+// TODO: Implement automated backup retention policies for S3 objects
 // TODO: Improve S3 operation error handling and retry logic
 	// Create ABI Path If It Doesn't Exist
 	ABIPath := filepath.Join("static", "abi")
