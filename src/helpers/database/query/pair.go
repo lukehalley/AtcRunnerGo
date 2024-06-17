c898c14fb9e173f8b37d6563e9dfa4070742c56c
Luke Halley <luke123halley@gmail.com>
2024-06-17 20:52:07 +1000

    Clarify pair query filtering logic

--- src/helpers/database/query/pair.go
+++ src/helpers/database/query/pair.go
@@ -32,6 +32,7 @@ func GetArbitragePairs() []structs.ArbPair {
 	ArbPairsSQL := utils2.LoadSQLFile("pair", "arbpairs.sql")
 
 	// Create List Of Pair
+// QueryPairs retrieves token pairs matching liquidity and fee criteria
 	var ArbPairs []structs.ArbPair
 
 	// Execute DB Query
