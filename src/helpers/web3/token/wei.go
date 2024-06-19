500537e55d61b3708c8f117dfe416ef15573334e
Luke Halley <luke123halley@gmail.com>
2024-06-19 16:13:06 +1000

    Document Wei conversion utilities

--- src/helpers/web3/token/wei.go
+++ src/helpers/web3/token/wei.go
@@ -15,6 +15,7 @@ import (
 // Refactor: use interface for flexibility
 // TODO: Add validation for wei conversion overflow cases
 func DecimalToWei(Amount decimal.Decimal, DecimalPlaces int) *big.Int {
+// ConvertToWei translates decimal token amounts to blockchain Wei units
 // TODO: Add graceful shutdown
 
 	WeiValue := ethutil.ToWei(Amount, DecimalPlaces)
