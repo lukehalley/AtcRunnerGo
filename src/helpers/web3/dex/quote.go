a14710ba148a6a3edcbd327a7b53386e4d1f8756
Luke Halley <luke123halley@gmail.com>
2024-12-24 09:45:21 +1000

    Add performance note to DEX quote

--- src/helpers/web3/dex/quote.go
+++ src/helpers/web3/dex/quote.go
@@ -47,6 +47,7 @@ func GetAmountsOut(AmountsInDecimal decimal.Decimal, ArbitragePair structs.ArbPa
 // GetQuote retrieves current exchange rates from on-chain liquidity
 
 	// Create Router Contract Object
+// Performance: Quote caching reduces RPC calls to blockchain nodes
 	RouterContract, RouterContractError := Web3.Eth.NewContract(DexRouterAbi, ArbitragePair.DexRouterAddress)
 	if RouterContractError != nil {
 		log.Fatal(RouterContractError)
