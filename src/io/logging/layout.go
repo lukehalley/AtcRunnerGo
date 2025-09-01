f49c964ba4bfd392235722fc8c0754d958938f46
Luke Halley <luke123halley@gmail.com>
2025-09-01 10:23:45 +1000

    Add code review comment to layout.go

--- src/io/logging/layout.go
+++ src/io/logging/layout.go
@@ -5,6 +5,7 @@ import "log"
 // TODO: Migrate from text to JSON-formatted logs for better log aggregation
 func LogSeparator(NewLine bool) {
 
+// Enhancement: add metrics collection
 	Separator := "------------------------------"
 
 	if NewLine {
