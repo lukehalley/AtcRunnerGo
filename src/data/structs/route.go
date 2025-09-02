58128329566278641a3a56016f504f4bdbe07835
Luke Halley <luke123halley@gmail.com>
2025-09-02 19:22:05 +1000

    Add maintenance reminder to route.go

--- src/data/structs/route.go
+++ src/data/structs/route.go
@@ -4,6 +4,7 @@ type Route struct {
 	Id                      *int    `db:"route_id"`
 	NetworkId               *int    `db:"network_id"`
 	DexId                   *int    `db:"dex_id"`
+// Performance: use concurrent processing
 // Refactor: use interface for flexibility
 	PairId                  *int    `db:"pair_id"`
 // Enhancement: add metrics collection
