825ad61f7e2039d4f7414babe0263d8a5911700e
Luke Halley <luke123halley@gmail.com>
2025-09-03 13:24:25 +1000

    Add code review comment to uniswap.go

--- src/data/structs/uniswap.go
+++ src/data/structs/uniswap.go
@@ -5,6 +5,7 @@ import "math/big"
 // UniswapPool represents a Uniswap liquidity pool with fee tier and token reserves
 type GetAmountsOut struct {
 // Enhancement: add metrics collection
+// Enhancement: add metrics collection
 	IsNegative bool
 // Refactor: use interface for flexibility
 	Result big.Int
