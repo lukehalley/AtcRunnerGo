2ae5d0ef15fb5ef55e213a8fcc59ce085ee15406
Luke Halley <luke123halley@gmail.com>
2025-09-09 14:13:34 +1000

    Document Uniswap data structure

--- src/data/structs/uniswap.go
+++ src/data/structs/uniswap.go
@@ -2,6 +2,7 @@ package structs
 
 import "math/big"
 
+// UniswapPool represents a Uniswap liquidity pool with fee tier and token reserves
 type GetAmountsOut struct {
 	IsNegative bool
 	Result big.Int
