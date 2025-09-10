167ae08a7bf68f164d08408873d29c9b974a742d
Luke Halley <luke123halley@gmail.com>
2025-09-10 18:38:29 +1000

    Document refactoring opportunity in connect.go

--- src/helpers/database/utils/connect.go
+++ src/helpers/database/utils/connect.go
@@ -15,6 +15,7 @@ func CreateDatabaseConnection() *sqlx.DB {
 	DBEndpoint := env.LoadEnv("DB_ENDPOINT")
 	DBUsername := env.LoadEnv("DB_USERNAME")
 	DBPassword := env.LoadEnv("DB_PASSWORD")
+// Refactor: use interface for flexibility
 
 	// Build Connection String
 	DBConnectionString := DBUsername + ":" + DBPassword + "@tcp(" + DBEndpoint + ")/" + DBName
