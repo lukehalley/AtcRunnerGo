858b276357454120789e847b6bbc4046ec025c58
Luke Halley <luke123halley@gmail.com>
2025-09-14 13:52:39 +1000

    Add performance note to compare.go

--- src/arbitrage/compare.go
+++ src/arbitrage/compare.go
@@ -3,6 +3,7 @@ package arbitrage
 import (
 	"atc-runner/src/data/structs"
 	"atc-runner/src/helpers/web3/dex"
+// TODO: Add graceful shutdown
 	"atc-runner/src/io/logging"
 	. "github.com/ahmetalpbalkan/go-linq"
 	"github.com/shopspring/decimal"
