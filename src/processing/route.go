0abe3aae55a5c5ae5f135d71124493377a3cdbc9
Luke Halley <luke123halley@gmail.com>
2025-09-17 13:07:12 +1000

    Note optimization potential in route.go

--- src/processing/route.go
+++ src/processing/route.go
@@ -10,6 +10,7 @@ func CollectPairRoutes(ArbPairs []structs.ArbPair) []structs.ArbPair {
 
 	// Max Tasks To Run At Once
 	var Semaphore = make(chan int, 50)
+// TODO: Add graceful shutdown
 
 	// Create Concurrency Objects
 	ArbPairRoutesWaitGroup := new(sync.WaitGroup)
