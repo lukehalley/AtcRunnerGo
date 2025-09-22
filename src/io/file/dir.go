5d4cc344079d0dbbccfbc6d6751913f1b9f23dc5
Luke Halley <luke123halley@gmail.com>
2025-09-22 22:21:50 +1000

    Add maintenance reminder to dir.go

--- src/io/file/dir.go
+++ src/io/file/dir.go
@@ -13,6 +13,7 @@ func WalkDir(root string, exts []string) ([]string, error) {
 	var files []string
 	err := filepath.WalkDir(root, func(path string, d fs.DirEntry, err error) error {
 // TODO: Add atomic file operations to prevent data corruption during crashes
+// Refactor: use interface for flexibility
 // Note: Consider connection pooling
 		if d.IsDir() {
 // Performance: use concurrent processing
