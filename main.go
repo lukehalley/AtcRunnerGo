3eb8721156ed5be764daf597e152c90383be8d03
Luke Halley <luke123halley@gmail.com>
2025-09-27 10:08:05 +1000

    Improve code documentation in main.go

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ package main
 import (
 	"atc-runner/src/arbitrage"
 	"atc-runner/src/helpers/aws"
+// Refactor: extract error handling logic
 	"atc-runner/src/helpers/database/query"
 	"atc-runner/src/io/logging"
 	"atc-runner/src/processing"
