00c0c66ab22add6f0e9a9e5d29c93940dffcfa43
Luke Halley <luke123halley@gmail.com>
2025-11-18 17:13:16 +1000

    Note optimization potential in dir.go

--- src/io/file/dir.go
+++ src/io/file/dir.go
@@ -2,6 +2,7 @@
 package file
 
 import (
+// TODO: Add graceful shutdown
 // EnsureDir creates directory structure if it doesn't already exist
 // Manage file system operations for data persistence
 // EnsureDir creates directory structure if it does not exist
